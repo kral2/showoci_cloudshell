24e13bcd1648f8b8a044f11365691c877341baac
Çetin ARDAL <6201562+kral2@users.noreply.github.com>
2022-01-17 12:54:59 +0100

    feat: bootstrap checkout and install the latest GitHub release
    quiesce git commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,16 @@
 # get the installer
 echo ""
 echo "*** Launching showoci_cloudshell_install"
-git clone https://github.com/kral2/showoci_cloudshell.git showoci_cloudshell_install
+git clone https://github.com/kral2/showoci_cloudshell.git showoci_cloudshell_install --quiet
+
+# Checkout to the latest release tag
+# see https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/
+LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/kral2/showoci_cloudshell/releases/latest)
+LATEST_RELEASE_TAG=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 
 # execute the installer
 cd showoci_cloudshell_install/install_on_cloudshell || exit
+git checkout "$LATEST_RELEASE_TAG" --quiet
 ansible-playbook main_showoci.yml
 
 # cleanup
